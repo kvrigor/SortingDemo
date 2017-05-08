984324adfc6331a0d8abf94e1e4e009244f767ae
kvrigor <pknethvgr@gmail.com>
2017-05-08 12:01:46 +0800

    Initial commit

---
+++ SortingDemo.cpp
@@ -0,0 +1,40 @@
+//============================================================================
+// Name        : SortingDemo.cpp
+// Authors     : chester.tan, leandro.umali, paul.rigor
+// Version     : v0.0
+// Description : Shell sort and quick sort comparison demo.
+// Git repo    :
+//============================================================================
+
+#include <iostream>
+#include <conio.h>
+#include "utils.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+// -----------------------  prototypes -----------------------
+void ShellSort(int [], int);
+void QuickSort(int [], int);
+
+
+int main()
+{
+	cout<<"Menu - TODO";
+	getch();
+	return 0;
+}
+
+// -----------------------  sorting algorithms -----------------------
+
+void ShellSort(int arr[], int size)
+{
+	//TODO
+}
+
+void QuickSort(int arr[], int size)
+{
+	//TODO
+}
---
+++ utils.h
@@ -0,0 +1,148 @@
+/*
+ * utils.h
+ *
+ *  Created on: Apr 15, 2017
+ *      Author: paul.rigor
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <windows.h>
+#include <ctime>
+#include <sstream>
+// Returns the amount of milliseconds elapsed since the UNIX epoch.
+// Source copied from http://stackoverflow.com/a/1861337/6592879
+typedef long long int64; typedef unsigned long long uint64;
+uint64 GetTimeMs64()
+{
+	/* Windows */
+	FILETIME ft;
+	LARGE_INTEGER li;
+
+	/* Get the amount of 100 nano seconds intervals elapsed since January 1, 1601 (UTC) and copy it
+	 * to a LARGE_INTEGER structure. */
+	GetSystemTimeAsFileTime(&ft);
+	li.LowPart = ft.dwLowDateTime;
+	li.HighPart = ft.dwHighDateTime;
+
+	uint64 ret = li.QuadPart;
+	ret -= 116444736000000000LL; /* Convert from file time to UNIX epoch time. */
+	ret /= 10000; /* From 100 nano seconds (10^-7) to 1 millisecond (10^-3) intervals */
+
+	return ret;
+}
+
+//Class encapsulating the measurement of elapsed time.
+class SimpleTimer
+{
+	private:
+		uint64 _startTime;
+		uint64 _accumulatedTime;
+		bool _timerStarted;
+	public:
+		SimpleTimer(bool startTimer = false)
+		{
+			_accumulatedTime = 0;
+			startTimer ? Start() : Reset();
+		}
+		void Start()
+		{
+			_startTime = GetTimeMs64();
+			_timerStarted = true;
+		}
+		void Reset()
+		{
+			_startTime = 0;
+			_timerStarted = false;
+			_accumulatedTime = 0;
+		}
+		void Pause()
+		{
+			if (_timerStarted)
+			{
+				_accumulatedTime += (GetTimeMs64() - _startTime);
+				_timerStarted = false;
+			}
+		}
+		void Restart()
+		{
+			Reset();
+			Start();
+		}
+		bool IsRunning() { return _timerStarted; }
+		uint64 Elapsed_ms()
+		{
+			if (_timerStarted)
+				return (_accumulatedTime + (GetTimeMs64() - _startTime));
+			else
+				return _accumulatedTime;
+		}
+		std::string Elapsed_ms_str()
+		{
+			std::stringstream elapsedTime;
+			elapsedTime<<Elapsed_ms();
+			return (elapsedTime.str() + " ms");
+		}
+
+};
+
+
+// Source copied from: http://www.cplusplus.com/articles/4z18T05o/#Windows
+void ClearScreen()
+{
+	HANDLE                     hStdOut;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	DWORD                      count;
+	DWORD                      cellCount;
+	COORD                      homeCoords = { 0, 0 };
+
+	hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+	if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+	/* Get the number of cells in the current buffer */
+	if (!GetConsoleScreenBufferInfo( hStdOut, &csbi )) return;
+	cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+	/* Fill the entire buffer with spaces */
+	if (!FillConsoleOutputCharacter(
+			hStdOut,
+			(TCHAR) ' ',
+			cellCount,
+			homeCoords,
+			&count
+	)) return;
+
+	/* Fill the entire buffer with the current colors and attributes */
+	if (!FillConsoleOutputAttribute(
+			hStdOut,
+			csbi.wAttributes,
+			cellCount,
+			homeCoords,
+			&count
+	)) return;
+
+	/* Move the cursor home */
+	SetConsoleCursorPosition( hStdOut, homeCoords );
+}
+
+void SetConsoleBufferHeight(int height)
+{
+	COORD coord;
+	coord.X = 80;
+	coord.Y = height;
+	HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleScreenBufferSize(Handle, coord);
+}
+
+bool IsKeyDown(int vKey)
+{
+	return (GetAsyncKeyState(vKey) & 0x8000);
+}
+
+uint64 GetRandomNumber(uint64 seedValue = GetTimeMs64())
+{
+	return seedValue+rand();
+}
+
+#endif /* UTILS_H_ */
